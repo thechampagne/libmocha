19f49bdb2abb3a8a5339acfd9e2e70025b90360e
XXIV <13811862+thechampagne@users.noreply.github.com>
2023-09-26 15:15:40 +0300

    add type to value

--- src/mocha.zig
+++ src/mocha.zig
@@ -7,14 +7,25 @@ const allocator = std.heap.c_allocator;
 //     MissingField, DuplicateField, RootReference, OutOfMemory, InvalidCharacter, Overflow, Core.Error = ?
 // };
 
+const mocha_value_type_t = enum(c_int) {
+    MOCHA_VALUE_TYPE_NIL,
+    MOCHA_VALUE_TYPE_STRING,
+    //MOCHA_VALUE_TYPE_REFERENCE,
+    MOCHA_VALUE_TYPE_BOOLEAN,
+    MOCHA_VALUE_TYPE_OBJECT,
+    MOCHA_VALUE_TYPE_ARRAY,
+    MOCHA_VALUE_TYPE_FLOAT64,
+    MOCHA_VALUE_TYPE_INTEGER64
+};
+
 const mocha_value_t = extern union {
     string: [*:0]const u8,
     //ref: Reference,
     boolean: bool,
     object: mocha_object_t,
     array: mocha_array_t,
-    float: f64,
-    integer: i64,
+    float64: f64,
+    integer64: i64,
 };
 
 const mocha_array_t = extern struct {
@@ -25,6 +36,7 @@ const mocha_array_t = extern struct {
 const mocha_field_t = extern struct {
     name: [*:0]const u8,
     value: mocha_value_t,
+    @"type": mocha_value_type_t,
 };
 
 const mocha_object_t = extern struct {
@@ -45,7 +57,36 @@ export fn mocha_deinit(object: *mocha_object_t) void {
 
 export fn mocha_field(object: *mocha_object_t, index: usize) mocha_field_t {
     const fields = fieldsCast(object.*.fields, object.*.fields_len);
-    return .{ .name = fields[index].name.ptr, .value = .{ .boolean = true} };
+    var value: mocha_value_t = undefined;
+    var @"type": mocha_value_type_t = undefined;
+    switch(fields[index].value) {
+        .string => |s| {
+            value = .{ .string = s.ptr };
+            @"type" = .MOCHA_VALUE_TYPE_STRING;
+        },
+        .boolean => |b| {
+            value = .{ .boolean = b };
+            @"type" = .MOCHA_VALUE_TYPE_BOOLEAN;
+        },
+        .object => |o| {
+            value = .{ .object = .{ .fields = o.fields.ptr, .fields_len = o.fields.len} };
+            @"type" = .MOCHA_VALUE_TYPE_OBJECT;
+        },
+        .array => |a| {
+            value = .{ .array = .{ .items = a.items.ptr, .items_len = a.items.len} };
+            @"type" = .MOCHA_VALUE_TYPE_ARRAY;
+        },
+        .float => |f| {
+            value = .{ .float64 = f };
+            @"type" = .MOCHA_VALUE_TYPE_FLOAT64;
+        },
+        .int => |i| {
+            value = .{ .integer64 = i };
+            @"type" = .MOCHA_VALUE_TYPE_INTEGER64;
+        },
+        else => @"type" = .MOCHA_VALUE_TYPE_NIL
+    }
+    return .{ .name = fields[index].name.ptr, .value = value, .@"type" = @"type" };
 }
 
 inline fn fieldsCast(fields: *anyopaque, fields_len: usize) []mocha.Field {
